40e221fa268a3e4faae959eff3d0eb998193cc4f
Louise Christiansen <89901683+Louigc@users.noreply.github.com>
2022-09-24 07:31:12 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,21 +6,29 @@ import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 import Home from './Components/Home';
 import About from './Components/About';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
        <NavbarComp></NavbarComp>
-        <Router>
           <Switch>
-              <Route path='/' exact component={Home}/>  
-              <Route path="/about" component={About}/> 
-              <Route path="/portfolio" component={Portfolio}/>
+              <Route exact path="/">
+                <Home />
+                </Route>  
+              <Route exact path="/about"> 
+                <About />
+              </Route>
+              <Route exact path="/contact">
+                <Contact />
+                </Route>
+              <Route exact path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="*">
+                <Redirect to="/" />
+              </Route>
           </Switch>
-        </Router>
       <Contact />
       <Footer />
     </>
--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -1,62 +1,37 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-
-import Home from './Home';
-import Contact from './Contact';
-import About from './About';
-import Portfolio from './Portfolio';
+import { Link } from "react-router-dom";
 import logo from '../Assets/img/logo-kopi.png';
-
 
 export default class NavbarComp extends Component {
     render() {
         return (
-            <Router>
+            <>
                 <div>
                     <Navbar variant={"dark"} expand="lg" className="navbar">
                     <Container className="navbarcon">
-                        <Navbar.Brand as={Link} to="/home">
+                        <Navbar.Brand as={Link} to="/">
                         <img src={logo} alt="Louise Guldbæk Christiansen logo" className="img-logo"></img>
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
-                            <Nav
-                                className="mr-auto my-2 my-lg-0 me-auto"
-                                style={{ maxHeight: '100px' }}
-                                navbarScroll
-                            >
-                                <Nav.Link as={Link} to="/home">Forside</Nav.Link>
-                                <Nav.Link as={Link} to="/about">Om mig</Nav.Link>
-                                <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+                            <Nav className="mr-auto my-2 my-lg-0 me-auto" style={{ maxHeight: '100px' }} navbarScroll>
+                                <Nav.Link as={Link} to="/">
+                                    Forside
+                                    </Nav.Link>
+                                <Nav.Link as={Link} to="/about">
+                                    Om mig
+                                    </Nav.Link>
+                                <Nav.Link as={Link} to="/portfolio">
+                                    Portfolio
+                                </Nav.Link>
                                 <Nav.Link href="#contact">Kontakt mig</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                      </Container>
                     </Navbar>
                 </div>
-                <div>
-                    <Switch>
-                    <Route path="/home">
-                            <Home />
-                        </Route>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/contact" >
-                            <Contact />
-                        </Route>
-                        <Route path="/portfolio">
-                            <Portfolio />
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
+            </>
         );
     }
 }
